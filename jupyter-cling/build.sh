02fc3c27aff760e3fe46e50a294dc48488c5e4f6
René Fonseca <rene.m.fonseca@gmail.com>
2020-03-05 22:24:05 +0100

    Updated config to support WebAssembly via wasmtime.

--- jupyter-cling/build.sh
+++ jupyter-cling/build.sh
@@ -3,7 +3,7 @@
 echo Building image for Jupyter with xeus/cling and BASE framework
 
 # --no-cache
-docker build --no-cache --build-arg VCS_REF=`git rev-parse --short HEAD` \
+docker build --build-arg VCS_REF=`git rev-parse --short HEAD` \
   --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
   --build-arg VERSION=1.0 \
   -t renefonseca/jupyter-cling:latest .
